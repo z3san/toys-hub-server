8b5a6314baa669dae3966499e6465ec41a2b08ac
z3san <ahtisamhaque@gmail.com>
2023-05-17 20:47:01 +0600

    mongodb is added and funtioning properly

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require("cors");
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -9,6 +10,39 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vzimcou.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    app.get('/', (req, res)=>{
+        res.send('ToysHub server is running ')
+    } )
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, ()=>{
     console.log(`ToysHub server is running on port ${port}`);
