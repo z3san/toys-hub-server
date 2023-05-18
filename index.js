93376072abce471ac259fbae1095bd7191c31686
z3san <ahtisamhaque@gmail.com>
2023-05-18 14:14:25 +0600

    toys data added on server

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db('toyCollection').collection('toysData')
+
     app.get('/', (req, res)=>{
         res.send('ToysHub server is running ')
     } )
