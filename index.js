15723822361b6838445ae11d606a340d948c22b5
z3san <ahtisamhaque@gmail.com>
2023-05-19 11:28:56 +0600

    my toys api is functioanl now

--- index.js
+++ index.js
@@ -64,6 +64,12 @@ async function run() {
   })
 
 
+  app.get("/myToys/:email", async (req, res)=>{
+   
+    console.log(req.params.email);
+    const result = await toysCollection.find({seller_email: req.params.email}).toArray()
+    res.send(result)
+  } )
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
